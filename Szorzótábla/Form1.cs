87e872057a0dba1868031c1370412d0f4f257da0
cnj2y8 <cnj2y8@SZA0030885>
2024-02-22 16:33:03 +0100

    Add project files.

---
+++ Szorzótábla/Form1.cs
@@ -0,0 +1,28 @@
+namespace Szorzótábla
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int sor = 0; sor < 10; sor++)
+            {
+                for (int oszlop = 0; oszlop < 10; oszlop++)
+                {
+                    Button button = new();
+                    button.Text = (sor * oszlop).ToString();
+
+                    Controls.Add(button);
+                    button.Top = sor*40;
+                    button.Left = oszlop *40;
+                    button.Height = 40;
+                    button.Width = 40;
+                }
+            }
+        }
+    }
+}
