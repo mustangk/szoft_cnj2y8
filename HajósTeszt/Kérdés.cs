e3b48e7cb70e75eab4a7381e0c0226c3e9c72cf6
Zsombor Kanyok <zsombor.kanyok@stud.uni-corvinus.hu>
2024-03-14 17:00:24 +0100

    4.hét

---
+++ HajósTeszt/Kérdés.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HajósTeszt
+{
+    internal class Kérdés
+    {
+        public string KérdésSzöveg { get; set; } = String.Empty;
+        public string Válasz1 { get; set; } = String.Empty;
+        public string Válasz2 { get; set; } = String.Empty;
+        public string Válasz3 { get; set; } = String.Empty;
+        public string URL { get; set; } = String.Empty;
+        public int HelyesVálasz { get; set; }
+        public int HelyesVálaszokSzáma { get; set; }
+    }
+}
