3ad5dcd3ac500d35979f2de8164b18eb8eac5ad7
Zsombor Kanyok <zsombor.kanyok@stud.uni-corvinus.hu>
2024-11-20 12:11:19 +0100

    password

--- Engineering/Adatbázis_feladat/Data/RendelesDbContext.cs
+++ Engineering/Adatbázis_feladat/Data/RendelesDbContext.cs
@@ -30,7 +30,7 @@ public partial class RendelesDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:zsombor.database.windows.net,1433;Initial Catalog=db;Persist Security Info=False;User ID=zsombor;Password=Zsombi123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("Server=tcp:zsombor.database.windows.net,1433;Initial Catalog=db;Persist Security Info=False;User ID=zsombor;Password=SoftEng2;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
